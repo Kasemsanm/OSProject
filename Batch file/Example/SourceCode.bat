33e0db33de9443b83f965db58326e9e710334186
kasemsanm <32940983+Kasemsanm@users.noreply.github.com>
2018-02-08 02:06:50 +0700

    Update dice game

--- Batch file/Example/SourceCode.bat
+++ Batch file/Example/SourceCode.bat
@@ -18,16 +18,50 @@ GOTO menu
 GOTO ExitAndMenu
 
 :DeleteFile
+cls
 REM Delete File but not delete file in except file
-ECHO
+ECHO -
 ECHO ============== Delete File ==============
 SET /P Except=Please Enter Except File :
-FOR %%i IN (*) DO IF NOT %%i == %Except% DEL "%%i"
+FOR %%i IN (*) DO IF NOT %%i == %Except% OR  DEL "%%i"
 REM End Delete file
 GOTO ExitAndMenu
 
 :DiceGame
+cls
+SET /P UserGuess=Please Enter guess score of two dice (2 to 12 score) :
+FOR %%i IN (2 3 4 5 6 7 8 9 10 11 12) DO IF %UserGuess% == %%i GOTO StartGame
+GOTO DiceGame
+:StartGame
+SET /A Rount=0
 
+:Dice
+SET /A Rount=%Rount%+1
+SET /A Dice1=%random% %%6+1
+SET /A Dice2=%random% %%6+1
+SET /A Score=%Dice1% + %Dice2%
+
+ECHO =================================
+ECHO   Rount of Dice : %Rount%
+ECHO   Dice1 is %Dice1%
+ECHO   Dice2 is %Dice2%
+ECHO   Dice Score : %Score%
+ECHO   UserGuess : %UserGuess%
+IF %Score% == %UserGuess%  GOTO endGame
+ECHO =================================
+ECHO ===    UserGuess Incorrect    ===
+ECHO =================================
+ECHO -
+ECHO -
+GOTO Dice
+
+:endGame
+ECHO =================================
+ECHO ===     UserGuess Correct     ===
+ECHO =================================
+ECHO -
+ECHO   Total Rount : %Rount%
+ECHO -
 GOTO ExitAndMenu
 
 :ExitAndMenu
