dd089261d28271bfaf602c4736eac4cbd7586605
kasemsanm <32940983+Kasemsanm@users.noreply.github.com>
2018-02-08 18:21:20 +0700

    co

--- Batch file/Example/SourceCode.bat
+++ Batch file/Example/SourceCode.bat
@@ -9,12 +9,30 @@ ECHO ##################################################
 SET /P choice=Please Enter choice :
 IF %choice% == 1 GOTO ShowDate
 IF %choice% == 2 GOTO DeleteFile
-IF %choice% == 3 GOTO DiceGame
+IF %choice% == 3 GOTO Guess Dice Game
 IF %choice% == e EXIT
 GOTO menu
 
 :ShowDate
-
+cls
+FOR /F "skip=1 tokens=1-6" %%A IN ('WMIC Path Win32_LocalTime Get Day^,Hour^,Minute^,Month^,Second^,Year /Format:table') DO (
+    IF NOT "%%~F"=="" (
+        SET /A SortDate = 10000 * %%F + 100 * %%D + %%A
+        set YEAR=!SortDate:~0,4!
+        set MON=!SortDate:~4,2!
+        set DAY=!SortDate:~6,2!
+        @REM Add 1000000 so as to force a prepended 0 if hours less than 10
+        SET /A SortTime = 1000000 + 10000 * %%B + 100 * %%C + %%E
+        set HOUR=!SortTime:~1,2!
+        set MIN=!SortTime:~3,2!
+        set SEC=!SortTime:~5,2!
+    )
+)
+@echo on
+@echo DATE=%DATE%, TIME=%TIME%
+@echo HOUR=!HOUR! MIN=!MIN! SEC=!SEC!
+@echo YR=!YEAR! MON=!MON! DAY=!DAY! 
+@echo DATECODE= '!YEAR!!MON!!DAY!!HOUR!!MIN!' 
 GOTO ExitAndMenu
 
 :DeleteFile
@@ -30,6 +48,8 @@ GOTO ExitAndMenu
 :DiceGame
 REM This Game is Guess Dice. User guess score of dice then computer dice. if score == Dice Computer stop Dice And Show Rount of Dice 
 cls
+ECHO -
+ECHO ============== Guess Dice Game ==============
 SET /P UserGuess=Please Enter guess score of two dice (2 to 12 score) :
 FOR %%i IN (2 3 4 5 6 7 8 9 10 11 12) DO IF %UserGuess% == %%i GOTO StartGame
 GOTO DiceGame
